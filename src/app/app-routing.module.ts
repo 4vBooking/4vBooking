6316f57682e1f3467ad37866c4cee8c33fb9a89e
EimarRamos <eimar.ramossacari@gmail.com>
2023-02-07 11:58:13 +0100

    bungalow-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: 'bungalow-list', pathMatch: 'full' },
   {
     path: 'bungalow-list',
     loadChildren: () => import('./bungalow-list/bungalow-list.module').then((m) => m.BungalowListModule),
